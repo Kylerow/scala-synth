15e11c9710d0abd326e5f30aa6f448168787619f
Kyle <kyle@kylerowlandmusic.com>
2013-09-28 12:58:01 -0700

    got the midi functions laid out - not working yet

--- src/main/scala/org/kylerow/App.scala
+++ src/main/scala/org/kylerow/App.scala
@@ -1,15 +1,42 @@
 package org.kylerow
 
+import javax.sound.midi.Receiver
+import javax.sound.midi.MidiMessage
+import javax.sound.midi.Transmitter
+import javax.sound.midi.MidiSystem
+import javax.sound.midi.MidiDevice
+import scala.collection.JavaConversions._
+
+case class Note(noteVal :Int);
+
 /**
- * @author ${user.name}
+ * @author KyleRow
  */
-object App {
+object ScalaSynth {
   
-  def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
+  def receiver( input :MidiMessage => Unit ) = {
+    new Receiver{
+      def send( msg :MidiMessage, timeStamp :Long ) = input(msg)
+      def close() = {}
+    }
+  }
+  
+  def transmitters :List[Transmitter] = {
+    for (
+      info <- MidiSystem.getMidiDeviceInfo().toList ;
+      transmitter <- MidiSystem.getMidiDevice(info).getTransmitters()
+      ) yield transmitter;
+  }
+  
+  def connect (tx :List[Transmitter], rx :Receiver) = tx.foreach({_.setReceiver(rx)});
+  
+  def receptacle (message :MidiMessage) = println("midi received: "+message.toString);
   
   def main(args : Array[String]) {
-    println( "Hello World!" )
-    println("concat arguments = " + foo(args))
+    connect (transmitters, receiver(receptacle));
+    while(true){
+      println("Waiting to quit...")
+      Thread.sleep(1000);
+    }
   }
-
 }

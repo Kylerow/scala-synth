36c2bf2c81cb8709faeb48955fa53be3e125907a
Kyle <kyle@kylerowlandmusic.com>
2013-10-17 10:05:24 -0700

    midi experiment is working...not a lot of unit tests: excuse: I suck - also, I barely can get the midi working...

--- src/main/scala/org/kylerow/App.scala
+++ src/main/scala/org/kylerow/App.scala
@@ -6,13 +6,35 @@ import javax.sound.midi.Transmitter
 import javax.sound.midi.MidiSystem
 import javax.sound.midi.MidiDevice
 import scala.collection.JavaConversions._
+import javax.sound.midi.MidiUnavailableException
 
 case class Note(noteVal :Int);
 
-/**
+/** Linux Notes:
+ *  	- Make sure virtual midi is setup: sudo modprobe snd-virmidi
+ *      - use patchage to connect virtual keyboard to the midi port (really any midi port:))
+ *   
  * @author KyleRow
  */
 object ScalaSynth {
+  
+  def transmitters :List[Transmitter] = {
+   // MidiSystem.getMidiDeviceInfo().toList.map(x => MidiSystem.getMidiDevice(x).open())
+    
+    
+    (for (info <- MidiSystem.getMidiDeviceInfo().toList;
+    	device = MidiSystem.getMidiDevice(info)) yield {
+        device open();
+        try { device getTransmitter } catch {
+          case un :MidiUnavailableException  => null;
+          case ex :Throwable => println("Exception: "+ ex);  null;
+        }
+      }) :::
+    (for (
+      info <- MidiSystem.getMidiDeviceInfo().toList;
+      transmitter <- MidiSystem.getMidiDevice(info).getTransmitters()
+      ) yield transmitter )
+  }
   
   def receiver( input :MidiMessage => Unit ) = {
     new Receiver{
@@ -21,22 +43,17 @@ object ScalaSynth {
     }
   }
   
-  def transmitters :List[Transmitter] = {
-    for (
-      info <- MidiSystem.getMidiDeviceInfo().toList ;
-      transmitter <- MidiSystem.getMidiDevice(info).getTransmitters()
-      ) yield transmitter;
-  }
-  
-  def connect (tx :List[Transmitter], rx :Receiver) = tx.foreach({_.setReceiver(rx)});
-  
   def receptacle (message :MidiMessage) = println("midi received: "+message.toString);
   
+  def connect (tx :List[Transmitter], rx :Receiver) = tx.filter(_!=null).foreach({ _.setReceiver(rx)});
+  
   def main(args : Array[String]) {
-    connect (transmitters, receiver(receptacle));
+    println("Connecting to All Virtual Midi...")
+    connect (transmitters, receiver(receptacle))
+    
+    println("Running...")
     while(true){
-      println("Waiting to quit...")
-      Thread.sleep(1000);
+      Thread.sleep(3000);
     }
   }
 }
---
+++ src/test/scala/org/kylerow/AppTest.scala
@@ -0,0 +1,5 @@
+package org.kylerow
+
+class AppTest {
+
+}

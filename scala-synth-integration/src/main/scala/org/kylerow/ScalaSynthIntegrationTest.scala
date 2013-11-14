8d4c9bc43d27153c8947ca8c165754b927934ec3
Kylerow <kylerow@gmail.com>
2013-11-14 12:40:44 -0800

    basic integration test working

--- scala-synth-integration/src/main/scala/org/kylerow/ScalaSynthIntegrationTest.scala
+++ scala-synth-integration/src/main/scala/org/kylerow/ScalaSynthIntegrationTest.scala
@@ -5,28 +5,28 @@ import org.scalatest.FlatSpec
 import org.scalatest.junit.JUnitRunner
 import javax.sound.midi.MidiSystem
 import org.scalamock.scalatest.MockFactory
+import javax.sound.midi.MidiMessage
+import org.kylerow.midi.Midi
+import javax.sound.midi.ShortMessage
 
 @RunWith(classOf[JUnitRunner])
 class ScalaSynthIntegrationTest 
 	extends FlatSpec 
-	with ShouldMatchers{
+	with ShouldMatchers
+	with MockFactory{
   
   "scala-synth" should "send midi messages to the receiver" in
   {
     // arrange
-	var syn = MidiSystem.getSynthesizer();
-	syn.open();
-	val mc = syn.getChannels();
-    val mockedReceiverFunction = MockFactory.mockFunction[MidiMessage,Unit];
+    val tx = MidiSystem.getTransmitter()
+    val mockedReceiverFunction = mockFunction[MidiMessage,Unit];
+    mockedReceiverFunction expects *;
     
     var midi = Midi()
     midi >>> mockedReceiverFunction
+    val rx = tx.getReceiver
     
     // act
-    mc(5).noteOn(50,1000);
-    
-    // assert
-    mockedReceiverFunction expects (new MidiMessage());
-    
+    rx.send(new ShortMessage(ShortMessage.NOTE_ON, 0, 60, 93),-1) 
   }
 }

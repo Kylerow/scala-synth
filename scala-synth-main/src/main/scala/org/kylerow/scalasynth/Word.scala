950c875df4b7d58b4a9c7f1080c6bc1b89b89ff1
Kylerow <kylerow@gmail.com>
2013-11-29 13:39:34 -0800

    setting up Word array conversion and sending feature of audio port

--- scala-synth-main/src/main/scala/org/kylerow/scalasynth/Word.scala
+++ scala-synth-main/src/main/scala/org/kylerow/scalasynth/Word.scala
@@ -3,4 +3,10 @@ package org.kylerow.scalasynth
 class  Word(val clz :Class[_], val value :Any);
 object Word{
   def apply(value :Byte) = {new Word(classOf[Byte],value);}
+  implicit class arrayWithBytes (array :Array[Word])
+  {	
+    def getBytes() :Array[Byte]= {
+    	null
+    } 
+  }
 }
--- scala-synth-main/src/main/scala/org/kylerow/scalasynth/audio/AudioPort.scala
+++ scala-synth-main/src/main/scala/org/kylerow/scalasynth/audio/AudioPort.scala
@@ -5,7 +5,9 @@ import javax.sound.sampled.DataLine
 
 class AudioPort {
 	def sendData(data :Array[Word]) = {
-	  
+	  // steps to glorious success:
+	  // 1) turn array[word] into array[byte]
+	  // 2) dataLine.write(buf, 0, BYTE_BUFFER_SIZE)
 	}
 	
 	var dataLine :DataLine = _;
